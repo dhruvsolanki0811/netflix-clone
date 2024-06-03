0c240516f8fcaf92e3bbca6dbdc85ffe26d14ac5
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2024-06-03 21:01:30 +0530

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./Provider";
 import 'react-toastify/dist/ReactToastify.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
     <Provider >{children}</Provider>
+    <Analytics />
       </body>
     </html>
   );
